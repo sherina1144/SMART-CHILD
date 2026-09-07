06a410cad5ddc43f9c95c8b38985b1d9ce0b1d2e
acell132 <cellostt132@gmail.com>
2026-09-07 21:23:30 +0700

    Membuat navbar SmartChild

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,58 @@
+```blade
+<header class="navbar">
+
+    <!-- LOGO -->
+    <div class="logo">
+        SMARTCHILD
+    </div>
+
+    <!-- MENU -->
+    <nav class="nav-menu">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+
+        <a href="#">
+            Development
+            <i class="bi bi-chevron-down"></i>
+        </a>
+
+        <a href="#">
+            Shop
+            <i class="bi bi-chevron-down"></i>
+        </a>
+
+        <a href="#">
+            Services
+            <i class="bi bi-chevron-down"></i>
+        </a>
+
+        <a href="#">Community</a>
+
+        <a href="#">
+            Partnership
+            <i class="bi bi-chevron-down"></i>
+        </a>
+
+        <a href="#">Contact</a>
+    </nav>
+
+    <!-- ICON KANAN -->
+    <div class="nav-icons">
+
+        <div class="search">
+            <i class="bi bi-search"></i>
+        </div>
+
+        <div class="cart">
+            <i class="bi bi-cart3"></i>
+            <small>0</small>
+        </div>
+
+        <div class="profile">
+            <i class="bi bi-person"></i>
+        </div>
+
+    </div>
+
+</header>
+```
